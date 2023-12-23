66d03c87ceec428510caf742998fefd2b0654b89
Kumodatsu <dragonkumo@gmail.com>
2023-12-23 16:06:08 +0100

    Use new Window classes

--- projects/playground/src/Main.cpp
+++ projects/playground/src/Main.cpp
@@ -1,6 +1,11 @@
-#include <pipsqueak/Common.hpp>
+#include <pipsqueak/Window.hpp>
 
 int main(int, char**) {
-  pipsqueak::test();
+  auto window = pipsqueak::create_window("Pipsqueak", 640, 480);
+  while (!window->should_close()) {
+    window->update();
+    window->swap_buffers();
+    window->poll_events();
+  }
   return 0;
 }
