bfd97de78d5620f848f411ad680d9c4531c7fc0a
Kumodatsu <dragonkumo@gmail.com>
2023-12-24 15:23:31 +0100

    Add Playground class to test App

--- projects/playground/src/Main.cpp
+++ projects/playground/src/Main.cpp
@@ -1,11 +1,29 @@
-#include <pipsqueak/Window.hpp>
+#include <iostream>
+#include <memory>
+#include <pipsqueak/App.hpp>
+
+using namespace pipsqueak;
+
+class Playground : public App {
+public:
+protected:
+  void on_update(std::chrono::duration<F32> delta) override {
+    ++m_update_count;
+    m_elapsed += delta;
+    static constexpr auto one_second = decltype(m_elapsed)(1.0f);
+    if (m_elapsed >= one_second) {
+      m_elapsed -= one_second;
+      std::cout << "FPS: " << m_update_count << std::endl;
+      m_update_count = 0;
+    }
+  }
+private:
+  I32 m_update_count = 0;
+  std::chrono::duration<F32> m_elapsed {0};
+};
 
 int main(int, char**) {
-  auto window = pipsqueak::create_window("Pipsqueak", 640, 480);
-  while (!window->should_close()) {
-    window->update();
-    window->swap_buffers();
-    window->poll_events();
-  }
+  std::unique_ptr<App> app = std::make_unique<Playground>();
+  app->run();
   return 0;
 }
