1eae23c02108b9d8414f5e3f1d87c31f723e358f
Kumodatsu <dragonkumo@gmail.com>
2024-03-02 23:03:49 +0100

    WIP

--- projects/pipsqueak/include/pipsqueak/math/Transform.hpp
+++ projects/pipsqueak/include/pipsqueak/math/Transform.hpp
@@ -19,6 +19,11 @@ namespace pipsqueak::math {
     }
 
     [[nodiscard]]
+    inline static constexpr Transform identity() {
+      return Transform(Mat4x4::identity());
+    }
+
+    [[nodiscard]]
     inline static constexpr Transform translation(const Vec3& translation) {
       return Transform({
         {1.0f, 0.0f, 0.0f, 0.0f},
@@ -30,7 +35,7 @@ namespace pipsqueak::math {
 
     [[nodiscard]]
     inline static constexpr Transform scale(F32 scalar) {
-      return Transform(scalar * Mat4x4::identity());
+      return scale({scalar, scalar, scalar});
     }
 
     [[nodiscard]]
@@ -39,7 +44,7 @@ namespace pipsqueak::math {
         scales.x, 0.0f,     0.0f,     0.0f,
         0.0f,     scales.y, 0.0f,     0.0f,
         0.0f,     0.0f,     scales.z, 0.0f,
-        0.0f,     0.0f,     0.0f,     0.0f,
+        0.0f,     0.0f,     0.0f,     1.0f,
       });
     }
 
@@ -49,8 +54,8 @@ namespace pipsqueak::math {
       const F32 sin = math::sin(angle);
       return Transform({
         1.0f, 0.0f, 0.0f, 0.0f,
-        0.0f,  cos,  sin, 0.0f,
-        0.0f, -sin,  cos, 0.0f,
+        0.0f,  cos, -sin, 0.0f,
+        0.0f,  sin,  cos, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f,
       });
     }
@@ -60,9 +65,9 @@ namespace pipsqueak::math {
       const F32 cos = math::cos(angle);
       const F32 sin = math::sin(angle);
       return Transform({
-         cos, 0.0f, -sin, 0.0f,
+         cos, 0.0f,  sin, 0.0f,
         0.0f, 1.0f, 0.0f, 0.0f,
-         sin, 0.0f,  cos, 0.0f,
+        -sin, 0.0f,  cos, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f,
       });
     }
@@ -72,30 +77,51 @@ namespace pipsqueak::math {
       const F32 cos = math::cos(angle);
       const F32 sin = math::sin(angle);
       return Transform({
-         cos,  sin, 0.0f, 0.0f,
-        -sin,  cos, 0.0f, 0.0f,
+         cos, -sin, 0.0f, 0.0f,
+         sin,  cos, 0.0f, 0.0f,
         0.0f, 0.0f, 1.0f, 0.0f,
         0.0f, 0.0f, 0.0f, 1.0f,
       });
     }
 
     [[nodiscard]]
+    inline static Transform orthographic(
+      const Vec3& left_bottom_near,
+      const Vec3& right_top_far
+    ) {
+      const Vec3
+        bounds      = right_top_far - left_bottom_near,
+        translation = -(right_top_far + left_bottom_near) / bounds;
+      return Transform({
+        2.0f / bounds.x,            0.0f,            0.0f, 0.0f,
+                   0.0f, 2.0f / bounds.y,            0.0f, 0.0f,
+                   0.0f,            0.0f, 2.0f / bounds.z, 0.0f,
+          translation.x,   translation.y,   translation.z, 1.0f,
+      });
+    }
+
+    [[nodiscard]]
     inline Vec2 transform(const Vec2& vector) const {
       const Vec4 transformed = m_matrix * Vec4{vector.x, vector.y, 0.0f, 1.0f};
-      return {transformed.x, transformed.y};
+      return Vec2 {transformed.x, transformed.y} / transformed.w;
     }
 
     [[nodiscard]]
     inline Vec3 transform(const Vec3& vector) const {
       const Vec4 transformed =
         m_matrix * Vec4{vector.x, vector.y, vector.z, 1.0f};
-      return {transformed.x, transformed.y, transformed.z};
+      return Vec3 {transformed.x, transformed.y, transformed.z} / transformed.w;
+    }
+
+    [[nodiscard]]
+    inline Transform then(const Transform& other) const {
+      return Transform(other.matrix() * matrix());
     }
 
     [[nodiscard]]
     inline friend Transform operator >> (const Transform& lhs, const Transform& rhs) {
-        return Transform(rhs.matrix() * lhs.matrix());
-    } 
+      return lhs.then(rhs);
+    }
   private:
     Mat4x4 m_matrix;
   };
--- tests/math_tests/TestTransform.cpp
+++ tests/math_tests/TestTransform.cpp
@@ -22,6 +22,53 @@ static constexpr Vec3
 
 TEST(
   TestTransform,
+  identity_transformation_does_not_transform_vector
+) {
+  const auto transform = Transform::identity();
+  const Vec3
+    vector {1.0f, -2.0f, 3.0f},
+    transformed = transform.transform(vector);
+  EXPECT_EQ(transformed, vector);
+}
+
+TEST(
+  TestTransform,
+  translation_moves_vector
+) {
+  const auto transform = Transform::translation({5.0f, -3.0f, 10.0f});
+  const Vec3
+    vector {1.0f, 2.0f, -3.0f},
+    transformed = transform.transform(vector),
+    expected {6.0f, -1.0f, 7.0f};
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  scale_with_scalar_transforms_vector_uniformly
+) {
+  const auto transform = Transform::scale(3.0f);
+  const Vec3
+    vector {1.0f, -2.0f, 3.0f},
+    transformed = transform.transform(vector),
+    expected {3.0f, -6.0f, 9.0f};
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  scale_with_vector_transforms_vector_componentwise
+) {
+  const auto transform = Transform::scale({10.0f, 20.0f, 30.0f});
+  const Vec3
+    vector {1.0f, -2.0f, 3.0f},
+    transformed = transform.transform(vector),
+    expected {10.0f, -40.0f, 90.0f};
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
   positive_x_rotated_half_pi_rad_around_x_results_in_positive_x
 ) {
   const auto transform = Transform
@@ -48,23 +95,10 @@ TEST(
 
 TEST(
   TestTransform,
-  positive_y_rotated_half_pi_rad_around_x_results_in_negative_z
+  positive_y_rotated_half_pi_rad_around_x_results_in_positive_z
 ) {
   const auto transform = Transform
     ::euler_rotation_x(0.5f * constants::pi<F32>);
-  const Vec3
-    vector      = positive_y,
-    transformed = transform.transform(vector),
-    expected    = negative_z;
-  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
-}
-
-TEST(
-  TestTransform,
-  positive_y_rotated_negative_half_pi_rad_around_x_results_in_positive_z
-) {
-  const auto transform = Transform
-    ::euler_rotation_x(-0.5f * constants::pi<F32>);
   const Vec3
     vector      = positive_y,
     transformed = transform.transform(vector),
@@ -74,23 +108,23 @@ TEST(
 
 TEST(
   TestTransform,
-  positive_z_rotated_half_pi_rad_around_x_results_in_positive_y
+  positive_y_rotated_negative_half_pi_rad_around_x_results_in_negative_z
 ) {
   const auto transform = Transform
-    ::euler_rotation_x(0.5f * constants::pi<F32>);
+    ::euler_rotation_x(-0.5f * constants::pi<F32>);
   const Vec3
-    vector      = positive_z,
+    vector      = positive_y,
     transformed = transform.transform(vector),
-    expected    = positive_y;
+    expected    = negative_z;
   EXPECT_VEC3_NEAR(transformed, expected, epsilon);
 }
 
 TEST(
   TestTransform,
-  positive_z_rotated_negative_half_pi_rad_around_x_results_in_negative_y
+  positive_z_rotated_half_pi_rad_around_x_results_in_negative_y
 ) {
   const auto transform = Transform
-    ::euler_rotation_x(-0.5f * constants::pi<F32>);
+    ::euler_rotation_x(0.5f * constants::pi<F32>);
   const Vec3
     vector      = positive_z,
     transformed = transform.transform(vector),
@@ -100,14 +134,214 @@ TEST(
 
 TEST(
   TestTransform,
-  positive_y_rotated_around_positive_x_then_positive_y_results_in_negative_x
+  positive_z_rotated_negative_half_pi_rad_around_x_results_in_positive_y
 ) {
-  const auto transform
-    =  Transform::euler_rotation_x(0.5f * constants::pi<F32>)
-    >> Transform::euler_rotation_y(0.5f * constants::pi<F32>);
+  const auto transform = Transform
+    ::euler_rotation_x(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_z,
+    transformed = transform.transform(vector),
+    expected    = positive_y;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_x_rotated_half_pi_rad_around_y_results_in_negative_z
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_x,
+    transformed = transform.transform(vector),
+    expected    = negative_z;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_x_rotated_negative_half_pi_rad_around_y_results_in_positive_z
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_x,
+    transformed = transform.transform(vector),
+    expected    = positive_z;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_y_rotated_half_pi_rad_around_y_results_in_positive_y
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_y,
+    transformed = transform.transform(vector),
+    expected    = positive_y;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_y_rotated_negative_half_pi_rad_around_y_results_in_positive_y
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_y,
+    transformed = transform.transform(vector),
+    expected    = positive_y;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_z_rotated_half_pi_rad_around_y_results_in_positive_x
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_z,
+    transformed = transform.transform(vector),
+    expected    = positive_x;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_z_rotated_negative_half_pi_rad_around_y_results_in_negative_x
+) {
+  const auto transform = Transform
+    ::euler_rotation_y(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_z,
+    transformed = transform.transform(vector),
+    expected    = negative_x;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_x_rotated_half_pi_rad_around_z_results_in_positive_y
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_x,
+    transformed = transform.transform(vector),
+    expected    = positive_y;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_x_rotated_negative_half_pi_rad_around_z_results_in_negative_y
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_x,
+    transformed = transform.transform(vector),
+    expected    = negative_y;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_y_rotated_half_pi_rad_around_z_results_in_negative_x
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(0.5f * constants::pi<F32>);
   const Vec3
     vector      = positive_y,
     transformed = transform.transform(vector),
     expected    = negative_x;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_y_rotated_negative_half_pi_rad_around_z_results_in_positive_x
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_y,
+    transformed = transform.transform(vector),
+    expected    = positive_x;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_z_rotated_half_pi_rad_around_z_results_in_positive_z
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_z,
+    transformed = transform.transform(vector),
+    expected    = positive_z;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  positive_z_rotated_negative_half_pi_rad_around_z_results_in_positive_z
+) {
+  const auto transform = Transform
+    ::euler_rotation_z(-0.5f * constants::pi<F32>);
+  const Vec3
+    vector      = positive_z,
+    transformed = transform.transform(vector),
+    expected    = positive_z;
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  points_transformed_by_orthographic_projection
+) {
+  const auto transform = Transform
+    ::orthographic({-1.0f, -1.0f, -1.0f}, {1.0f, 1.0f, 1.0f});
+  const Vec3
+    vector      = {0.5f, 0.5f, 0.5f},
+    transformed = transform.transform(vector),
+    expected    = vector;
+  EXPECT_VEC3_NEAR(vector, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  composition_with_operator_of_two_transformations_composes_left_to_right
+) {
+  const auto transform
+    =  Transform::identity()
+    >> Transform::euler_rotation_x(0.5f  * constants::pi<F32>)
+    >> Transform::translation({1.0f, 1.0f, 0.0f})
+    >> Transform::scale(-0.5f);
+  const Vec3
+    vector      = positive_y,
+    transformed = transform.transform(vector),
+    expected    = {-0.5f, -0.5f, -0.5f};
+  EXPECT_VEC3_NEAR(transformed, expected, epsilon);
+}
+
+TEST(
+  TestTransform,
+  composition_with_then_of_two_transformations_composes_left_to_right
+) {
+  const auto transform
+    = Transform::identity()
+    .then(Transform::euler_rotation_x(0.5f  * constants::pi<F32>))
+    .then(Transform::translation({1.0f, 1.0f, 0.0f}))
+    .then(Transform::scale(-0.5f));
+  const Vec3
+    vector      = positive_y,
+    transformed = transform.transform(vector),
+    expected    = {-0.5f, -0.5f, -0.5f};
   EXPECT_VEC3_NEAR(transformed, expected, epsilon);
 }
